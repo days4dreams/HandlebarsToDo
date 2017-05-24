465a5ee23becf96e021af8b046a18e24d328fc7e
Lauren Wilson <emaillaurenwilson@gmail.com>
2017-05-24 22:00:06 +0100

    Requires .JS and CSS files to be within /css or / js folders
    // When the user submits the form, do:
      // Prevents the default action
      // Creates an object data; 
      			// item: the value the user entered into the #item input (hint: use the val() method)
      // Get HTML from #grocery-template, store it in var source
      // Use Handlebars.compile() to compile source template and store in var template
      // Use template() to add data to the template. Store in var newListItemHTML
      // Append newListItemHTML to groceries list

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-using Handlebars framework with jQuery to update the html of a simple To Do list webpage via a template.
-demonstrates use of Seperation of Concerns.
-
 // When the user submits the form, do:
-  // Prevents the default action
+	// Prevents the default action
   // Creates an object data; 
   			// item: the value the user entered into the #item input (hint: use the val() method)
   // Get HTML from #grocery-template, store it in var source
   // Use Handlebars.compile() to compile source template and store in var template
   // Use template() to add data to the template. Store in var newListItemHTML
   // Append newListItemHTML to groceries list
+  
+  $('#submitBtn').on('click', function(e){
+    e.preventDefault();
+
+    var source = $('#grocery-template').html();
+
+    var template = Handlebars.compile(source);
+
+    var data = {newItem: $('#item').val()};
+
+    var newListItemHTML = template(data);
+
+    $('#groceries').append(newListItemHTML);
+
+  });
